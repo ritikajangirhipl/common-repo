fb484ab82f725d54e73000d2452e9c66b5ae4225
Ritika Jangir <ritikajangir.hipl@gmail.com>
2025-01-02 12:40:49 +0530

    if condition for helper function

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Str;
 
-function generateRandomString($length = 6){
-    $randomString = Str::random($length);
-    return $randomString;
+if (!function_exists('generateRandomString')) {
+    function generateRandomString($length = 6){
+        $randomString = Str::random($length);
+        return $randomString;
+    }
 }
